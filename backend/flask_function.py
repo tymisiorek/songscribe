25ada644ab1a185eb825858bf360dcbe886f91b8
Ty Misiorek <zem4by@virginia.edu>
2024-06-28 15:32:18 -0400

    callback+login

--- backend/flask_function.py
+++ backend/flask_function.py
@@ -1,9 +1,6 @@
-from dotenv import load_dotenv
-import os
-import base64
 import requests
-import json
-from flask import Flask
+from flask import Flask, redirect
+import urllib.parse
 
 
 app = Flask(__name__)
@@ -11,8 +8,8 @@ app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://localhost:5000/callback"
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
@@ -21,3 +18,18 @@ TOKEN_URL = 'https://accounts.spotify.com/api/token'
 @app.route('/')
 def index(): 
     return "Songscribe <a href = '/login>Login With Spotify:</a>"
+
+@app.route('/login')
+def login():
+    scope = 'user-read-private user-read-email'
+
+    params = {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI,
+        'show_dialog': True
+    }
+
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    return redirect(auth_url)
