99050fef8ac40b39e1cb20fffac716116614f228
Ty Misiorek <zem4by@virginia.edu>
2024-07-17 16:00:48 -0400

    chang

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -6,9 +6,21 @@ import re
 import sys
 from dotenv import load_dotenv
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+
+
 
 #env and track dataframe
 load_dotenv()
+USER_DATA = os.getenv("FEATURES_PATH")
+user_dataset = pd.read_csv(USER_DATA)
+user_dataset['user_song'] = 1
+
 DATASET_PATH = os.getenv("DATASET_PATH")
 track_dataset1 = pd.read_csv(DATASET_PATH + "spotify_tracks_data.csv")
 track_dataset2 = pd.read_csv(DATASET_PATH + "spotify_tracks_data_2018.csv")
@@ -20,13 +32,25 @@ filtered_dataset1 = track_dataset1[common_columns]
 filtered_dataset2 = track_dataset2[common_columns]
 filtered_dataset3 = track_dataset3[common_columns]
 merged_dataset = pd.concat([filtered_dataset1, filtered_dataset2, filtered_dataset3])
+merged_dataset['user_song'] = 0
+merged_dataset = pd.concat([merged_dataset, user_dataset], ignore_index=True)
 merged_dataset = merged_dataset.drop_duplicates(subset='track_id').reset_index(drop=True)
 
-#minmax scale
-song_features = ['popularity', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
-scaler = MinMaxScaler()
-merged_dataset[song_features] = scaler.fit_transform(merged_dataset[song_features])
 
+#minmax scale
+song_features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+X = user_dataset[song_features]
+
+# Clustering
+kmeans = KMeans(n_clusters=10, random_state=42)
+user_dataset['cluster'] = kmeans.fit_predict(X)
+
+# Plot clusters
+plt.scatter(user_dataset['danceability'], user_dataset['energy'], c=user_dataset['cluster'])
+plt.xlabel('Danceability')
+plt.ylabel('Energy')
+plt.title('Song Clusters')
+plt.show()
 
 
 path = DATASET_PATH + "full_track_data.csv"
--- scripts/recommendation_engine.py
+++ scripts/recommendation_engine.py
@@ -9,7 +9,9 @@ from dotenv import load_dotenv
 #env and track dataframe
 load_dotenv()
 DATASET_PATH = os.getenv("DATASET_PATH")
-track_dataset = pd.read_csv(DATASET_PATH)
+USER_DATA = os.getenv("FEATURES_PATH")
+track_dataset = pd.read_csv(DATASET_PATH, "spotify_tracks_data.csv")
+user_dataset = pd.read_csv(USER_DATA)
 genres = set(list(track_dataset["track_genre"]))
 print(genres)
 
