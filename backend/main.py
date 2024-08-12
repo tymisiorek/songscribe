9b9f941a6c248813628c604551c8a56b837978be
Ty Misiorek <zem4by@virginia.edu>
2024-08-12 01:48:27 -0400

    remove

--- backend/main.py
+++ backend/main.py
@@ -17,9 +17,9 @@ app.secret_key = os.getenv("SECRET_KEY")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://localhost:5000/callback"
-AUTH_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
-API_BASE_URL =  'https://api.spotify.com/v1/'
+# AUTH_URL = 'https://accounts.spotify.com/authorize'
+# TOKEN_URL = 'https://accounts.spotify.com/api/token'
+# API_BASE_URL =  'https://api.spotify.com/v1/'
 
 
 @app.route('/')
